ac9f7821367f3f10ca5c12a9b46bf29f067e27e5
NunzioDiCroce <nunziodavide@gmail.com>
2023-07-04 18:53:10 +0200

    Commit, esercizio3 work in progress

--- src/Esercizio3_Package/Esercizio3Main.java
+++ src/Esercizio3_Package/Esercizio3Main.java
@@ -2,32 +2,71 @@ package Esercizio3_Package;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Esercizio3Main {
 
 	public static void main(String[] args) {
 
-		Map<String, String> rubrica = new HashMap<>();
+		Map<String, String> rubrica = new HashMap<>(); // HashMap object definition
+
+		Scanner esercizioTre = new Scanner(System.in);
+
+		System.out.println("");
+		System.out.println("- - - - - - - - - - ESERCIZIO 3 - - - - - - - - - -");
+		System.out.println("");
+
+		System.out.println(" Operazioni possibili");
+		System.out.println("");
+		System.out.println(" 1) Inserire nuovo contatto");
+		System.out.println(" 2) Cancella contatto esistente");
+		System.out.println(" 3) Cerca telefono contatto");
+		System.out.println(" 4) Cerca nome contatto");
+		System.out.println(" 5) Stampa contatti");
+		System.out.println("");
+		System.out.println(" Scegli un numero da 1 a 5:");
+
+		String scelta = esercizioTre.nextLine();
+
+		String sceltaSwitch = scelta;
+
+		switch (sceltaSwitch) {
+		case "1":
+			inserisciContatto();
+			break;
+		case "2":
+			cancellaContatto();
+			break;
+		case "3":
+			cercaTelefono();
+			break;
+		case "4":
+			cercaNome();
+			break;
+		case "5":
+			stampaContatti();
+			break;
+		}
 
 	}
 
-	public static void firstFunction() {
+	public static void inserisciContatto() {
 
 	}
 
-	public static void secondFunction() {
+	public static void cancellaContatto() {
 
 	}
 
-	public static void thirdFunction() {
+	public static void cercaTelefono() {
 
 	}
 
-	public static void fourthFunction() {
+	public static void cercaNome() {
 
 	}
 
-	public static void fifthFunction() {
+	public static void stampaContatti() {
 
 	}
 

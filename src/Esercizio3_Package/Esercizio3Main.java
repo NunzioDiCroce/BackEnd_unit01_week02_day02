704f092f4b38478cf84ce72bf1cc302932708cf9
NunzioDiCroce <nunziodavide@gmail.com>
2023-07-04 16:57:53 +0200

    Commit, esercizio3 work in progress

--- src/Esercizio3_Package/Esercizio3Main.java
+++ src/Esercizio3_Package/Esercizio3Main.java
@@ -1,8 +1,33 @@
 package Esercizio3_Package;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Esercizio3Main {
 
 	public static void main(String[] args) {
+
+		Map<String, String> rubrica = new HashMap<>();
+
+	}
+
+	public static void firstFunction() {
+
+	}
+
+	public static void secondFunction() {
+
+	}
+
+	public static void thirdFunction() {
+
+	}
+
+	public static void fourthFunction() {
+
+	}
+
+	public static void fifthFunction() {
 
 	}
 
